d0f21aed71c8e6006d1171a53ff6f726ab00aaec
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:59:12 +1300

    Converted to PSR-2

--- code/dataobjects/BlockTranslation.php
+++ code/dataobjects/BlockTranslation.php
@@ -1,27 +1,29 @@
 <?php
-class BlockTranslation extends DataObject {
-	private static $db = [
-        'Title'		=> 'Varchar',
-		'Content' 	=> 'HTMLText',
-		'Language' 	=> 'Varchar'
-	];
+class BlockTranslation extends DataObject
+{
+    private static $db = [
+        'Title'        => 'Varchar',
+        'Content'    => 'HTMLText',
+        'Language'    => 'Varchar'
+    ];
 
-	private static $has_one = array(
-		'Block' => 'Block'
-	);
+    private static $has_one = array(
+        'Block' => 'Block'
+    );
 
-	function forTemplate() {
-		$block = Block::get()->filter(['ID' => $this->BlockID])->first();
-			
-		return $this->renderWith([$block->Template, 'Block']); // Fall back to Block if selected does not exist
-	}
+    public function forTemplate()
+    {
+        $block = Block::get()->filter(['ID' => $this->BlockID])->first();
+            
+        return $this->renderWith([$block->Template, 'Block']); // Fall back to Block if selected does not exist
+    }
 
-	public function Images()
-	{
-		if ($this->Block()->Images()->Count() >= 1) {
-			return $this->Block()->Images();
-		}
+    public function Images()
+    {
+        if ($this->Block()->Images()->Count() >= 1) {
+            return $this->Block()->Images();
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
